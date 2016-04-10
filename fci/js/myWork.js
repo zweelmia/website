35e3979117cfb546654e13729f534a23c5e47363
zweel mia <zweelmia9@gmail.com>
2016-04-10 04:26:29 -0700

    final project
    this is new  site

---
+++ fci/js/myWork.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+     $(".main_menu_responsive_icon").click(function(){
+        $(".nav ul").slideToggle();  
+     });
+     
+     $('.carousel').carousel({
+       interval:2000
+     })
+});
